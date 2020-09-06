87f08b826311ebcef45fae34ce1249fa59b78a18
Martin Blahutka <blahec@Martins-MacBook-Pro.local>
2020-09-05 20:55:53 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"; // useState = this.setState in CBC
+import "./App.css";
+
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>;
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn React",
+      inCompleted: false,
+    },
+    {
+      text: "Meet friend",
+      inCompleted: false,
+    },
+    {
+      text: "Build Todo app",
+      inCompleted: false,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
